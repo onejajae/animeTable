f8890b3c8e1d85957945dd97b2113028d8478782
onejajae <onejajae@gmail.com>
2017-08-17 20:54:08 +0900

    Add integrated anime day table maker function
    'make_day_table' function return dictionary.
    The dict includes anissia and ohli server status.
    Also it includes integrated table information list.

--- src/animeTable/daytable.py
+++ src/animeTable/daytable.py
@@ -1,6 +1,24 @@
 import src.api.anissia.daytable as anissia
 import src.api.ohli.daytable as ohli
 
-if __name__ == "__main__":
-    print(anissia.get_day_table(0))
-    print(ohli.get_day_table(0))
+
+def make_day_table(day):
+    anissia_data = anissia.get_day_table(day)
+    ohli_data = ohli.get_day_table(day)
+
+    day_table = {'anissia_status': anissia_data['status'],
+                 'ohli_status': ohli_data['status'],
+                 'data': []}
+
+    if anissia_data['status'] == 'ok' and ohli_data['status'] != 'ok':
+        day_table['data'] == anissia_data['data']
+        return day_table
+    elif anissia_data['status'] != 'ok' and ohli_data['status'] == 'ok':
+        day_table['data'] == ohli_data['data']
+        return day_table
+    elif anissia_data['status'] == 'ok' and ohli_data['status'] == 'ok':
+        day_table['data'] == anissia_data['data']
+        return day_table
+    else:
+        day_table['data'] = None
+        return day_table
