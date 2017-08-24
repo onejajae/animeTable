f029a485579927aaaabbbfc095b8f3023ce27f5e
onejajae <onejajae@gmail.com>
2017-08-24 16:44:11 +0900

    Apply series number generator to anissia subtitle table api

--- src/api/anissia/subtitletable.py
+++ src/api/anissia/subtitletable.py
@@ -1,7 +1,9 @@
 from ..responsecheck import response_check
-
+from .seriesnumber import series_number
 
 def get_subtitle_table(anime_code):
     url = "http://www.anissia.net/anitime/cap?i=" + str(anime_code)
+    subtitle_table_data = response_check(url)
+    subtitle_table_data['data'] = series_number(subtitle_table_data['data'])
 
-    return response_check(url)
+    return subtitle_table_data
