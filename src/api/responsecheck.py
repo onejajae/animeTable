dc1f442d9c3428b536c054a347d5bec1c847d395
onejajae <onejajae@gmail.com>
2017-08-24 16:12:02 +0900

    Add Excption occuring time out
    connection error and time out error will be able to
    have whole program time problem although I add excpetional case.

--- src/api/responsecheck.py
+++ src/api/responsecheck.py
@@ -2,7 +2,10 @@ import requests
 
 
 def response_check(url):
-    r = requests.get(url)
+    try:
+        r = requests.get(url)
+    except:
+        return {'status': 'request time out', 'data': None}
 
     if r.status_code == 200:
         try:
